e316b22884ee47a811ef4400bf33d2daa7c928df
shinopai <harbor.shinozaki@gmail.com>
2022-02-08 05:04:12 +0000

    excute photo seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        // $this->call(PhotosTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        $this->call(PhotosTableSeeder::class);
         // $this->call(LikesTableSeeder::class);
     }
 }
--- database/seeds/PhotosTableSeeder.php
+++ database/seeds/PhotosTableSeeder.php
@@ -12,6 +12,6 @@ class PhotosTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Photo::class, 1000)->create();
+        factory(Photo::class, 3000)->create();
     }
 }
